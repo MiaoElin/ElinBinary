82a3f25f951ba731d392cf6490ec76337cc5c972
MiaoElin <249176332@qq.com>
2024-02-02 23:08:50 +0800

    Establish

---
+++ Assets/Script/BinaryWriter.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ElinBinary {
+    public static class BinaryWriter {
+        public static void WriteInt() {
+
+        }
+    }
+}
