fdb68117c1008d3a6215ebf6d12824a9ebb6be6b
MiaoElin <249176332@qq.com>
2024-07-16 21:11:17 +0800

    <feature> add: Sample_Hex 世界坐标找GridCenter逻辑坐标的验证

--- Assets/Sample/Hex/Sample_Hex.cs
+++ Assets/Sample/Hex/Sample_Hex.cs
@@ -5,6 +5,8 @@ public class Sample_Hex : MonoBehaviour {
     public int gridHeight;
     public float outterRadius;
     public HexCell[] hexCells;
+    [SerializeField] Transform mouse;
+
 
     void Start() {
         hexCells = new HexCell[gridWidth * gridHeight];
@@ -18,15 +20,36 @@ public class Sample_Hex : MonoBehaviour {
         }
     }
 
-    void Update() {
+    void OnGUI() {
+
+        // Vector2 mousePos = Input.mousePosition;
+        // Vector3 mouseWordPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10));
+
+        Vector2 center = GFHex.WorldPosToGridPos2Int(new Vector2(mouse.position.x, mouse.position.z), outterRadius, hexCells);
+        GUILayout.Label($"Mouse Pos{center}");
+
+        if (hexCells == null) {
+            return;
+        }
         foreach (var hex in hexCells) {
-            // var worldPos = new Vector3(hex.worldPos.x, 0, hex.worldPos.y);
-            // Debug.DrawLine(hex.worldPos, hex.worldPos + Vector3.forward * outterRadius, Color.red);
+            Vector3 worldPos = hex.worldPos;
+            Vector3 screePos = Camera.main.WorldToScreenPoint(worldPos);
+            GUI.Label(new Rect(screePos.x - 1, Screen.height - screePos.y, 30, 20), $"({hex.gridPos2Int.x},{hex.gridPos2Int.y})");
+        }
+
+    }
+
+    void OnDrawGizmos() {
+        if (hexCells == null) {
+            return;
+        }
+        foreach (var hex in hexCells) {
             Vector3[] arround = hex.Get_ArroundWorldPOS(outterRadius);
             for (int i = 0; i < arround.Length; i++) {
                 Debug.DrawLine(arround[i], arround[(i + 1) % arround.Length], Color.blue);
-            }  
+            }
         }
+
     }
 
 }
--- Assets/ScriptRuntime/Hex/GFHex.cs
+++ Assets/ScriptRuntime/Hex/GFHex.cs
@@ -4,7 +4,7 @@ public static class GFHex {
 
     public static int GetIndex(int gridWidth, int gridHeight, Vector3Int gridPos) {
         // grid2Int y
-        int y = gridPos.y;
+        int y = gridPos.z;
         // y如果超出范围 返回-1
         if (y >= gridHeight || y < 0) {
             return -1;
@@ -53,16 +53,60 @@ public static class GFHex {
         Vector2Int result = guessPos;
         for (int x = min.x; x <= max.x; x++) {
             for (int y = min.y; y <= max.y; y++) {
-                Vector2Int cur = new Vector2Int(x, y);
+                Vector2Int curlogic = new Vector2Int(x, y);
+                Vector2 cur = Grid2IntToWorldPos(curlogic, outterRadius);
                 float distanceSqr = Vector2.SqrMagnitude(worldPos - cur);
                 if (distanceSqr < nearlyDistance) {
                     nearlyDistance = distanceSqr;
-                    result = cur;
+                    result = curlogic;
                 }
             }
         }
         return result;
     }
+
+    public static Vector2Int WorldPosToGridPos2Int(Vector2 worldPos, float outterRadius, HexCell[] hexCells) {
+        float innerRadius = outterRadius * Mathf.Sqrt(3) / 2;
+        Vector2Int guessPos = Vector2Int.zero;
+        guessPos.x = Mathf.CeilToInt(worldPos.x / (innerRadius * 2f));
+        guessPos.y = Mathf.CeilToInt(worldPos.y / (outterRadius * 1.5f));
+
+        Vector2Int min = new Vector2Int(guessPos.x - 1, guessPos.y - 1);
+        Vector2Int max = guessPos;
+        float nearlyDistance = float.MaxValue;
+        Vector2Int result = guessPos;
+        for (int x = min.x; x <= max.x; x++) {
+            for (int y = min.y; y <= max.y; y++) {
+                Vector2Int curlogic = new Vector2Int(x, y);
+                int index = GridIndex(10, 10, curlogic);
+                if (index == -1) {
+                    continue;
+                }
+                Vector3 cur = hexCells[index].worldPos;
+                float distanceSqr = Vector2.SqrMagnitude(worldPos - new Vector2(cur.x, cur.z));
+                if (distanceSqr < nearlyDistance) {
+                    nearlyDistance = distanceSqr;
+                    result = curlogic;
+                }
+            }
+        }
+        return result;
+    }
+
+
+
+    public static Vector2 Grid2IntToWorldPos(Vector2Int grid2Int, float outterRadius) {
+        Vector2 result;
+        float innerRadius = outterRadius * Mathf.Sqrt(3) / 2;
+        if (grid2Int.y % 2 == 0) {
+            result.x = grid2Int.x * innerRadius * 2f + innerRadius;
+        } else {
+            result.x = grid2Int.x * innerRadius * 2f;
+        }
+        result.y = outterRadius * 1.5f * grid2Int.y;
+        return result;
+    }
+
 
     // public static Vector3Int WorldPosToGridPos3Int(Vector3 worldPos, float outterRadius, int gridWidth) {
     //     // 在这个外层要先判断这个worldPos是否在范围内，不在的话，退出
