cdef08257fe55ec6d770376dc32f9c44beccd94a
MiaoElin <249176332@qq.com>
2024-09-22 21:35:21 +0800

    <feature> add: 相机缓动跟随

--- Assets/ScriptRuntime/Camera2D/CameraEntity.cs
+++ Assets/ScriptRuntime/Camera2D/CameraEntity.cs
@@ -1,15 +1,32 @@
 using UnityEngine;
+using GameFunctions;
 
 public class CameraEntity {
     public Camera main;
     Vector3 offset;
+    Vector3 startPos;
+    Vector3 endPos;
+    float duration;
+    float t;
 
     public CameraEntity(Camera camera) {
         main = camera;
         offset = main.transform.position;
+        duration = 0.2f;
     }
 
     public void Follow(Vector2 target) {
-        main.transform.position = offset + (Vector3)target;
+        Vector3 newPos = offset + (Vector3)target;
+        if (newPos != endPos) {
+            startPos = main.transform.position;
+            endPos = newPos;
+            t = 0;
+        }
+
+        if (t < duration) {
+            t += Time.deltaTime;
+            main.transform.position = GFEasing.Ease3D(GFEasingEnum.Linear, t, duration, startPos, endPos);
+        }
     }
+
 }
